0c0973ce122d22a33029cc2e3101addb515304d9
SeunghwanJoo <tmdghks584@gmail.com>
2016-05-24 19:37:53 +0900

    update views.py

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -1,3 +1,5 @@
+import requests
+
 from django.http.response import HttpResponse
 
 # MVC Controller
@@ -5,5 +7,11 @@ def home(request):
     return HttpResponse("hello world")
 
 def room(request, room_id):
-    return HttpResponse("This is a room detail " + room_id)
+    # 방 번호 ( room_id) 직방의 데이터를 그대로 보여주는 뷰(컨트롤러)
+    url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
+    response = requests.get(url)
+    return HttpResponse(
+        response.content,
+        content_type="application/json",
+    )
 
