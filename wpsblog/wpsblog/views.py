68bf9a117b7e04078a797d18bddc8fc7321ca0da
SeunghwanJoo <tmdghks584@gmail.com>
2016-05-25 15:04:55 +0900

    init home.html

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -6,7 +6,9 @@ from django.conf import settings
 
 # MVC Controller
 def home(request):
-    return HttpResponse("hello world")
+	with open(settings.BASE_DIR + "/wpsblog/templates/home.html","r") as template:
+		content = template.read()
+	return HttpResponse(content)
 
 def room(request, room_id):
 	# 방 번호 ( room_id) 직방의 데이터를 그대로 보여주는 뷰(컨트롤러)
@@ -23,7 +25,7 @@ def news(request):
 	response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
 	news_dict = json.loads(response.text)
 	news_list = news_dict.get("news")
-	
+
 	if search:
 		news_list = list(filter(
 				lambda news: search in news.get('title'),
