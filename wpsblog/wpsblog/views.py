55ce9ca2e48a5a49b3f7af15fa5db593943ecf80
SeunghwanJoo <tmdghks584@gmail.com>
2016-05-25 14:44:49 +0900

    init templates/news.html

--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 from django.http.response import HttpResponse
+from django.conf import settings
 
 # MVC Controller
 def home(request):
@@ -22,27 +23,21 @@ def news(request):
 	response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
 	news_dict = json.loads(response.text)
 	news_list = news_dict.get("news")
-
-	form_html = """
-		<form method="GET" action="/news/">
-			<input type="text" name="search">
-			<input type="submit" value="검색">
-		</form>
-	"""
-
+	
 	if search:
 		news_list = list(filter(
 				lambda news: search in news.get('title'),
 				news_list,
 		))
-	content = "<h1>News</h1>" +\
-			   "<p>This is news page.</p>" +\
-			   "".join([
-					   "<h2>{title}</h2><img src={image_url}><p>{content}</p>".format(
-						   title=news.get("title"),
-						   image_url=news.get('image'),
-						   content=news.get('content'),
-						   )
-					   for news in news_list
-					   ])
-	return HttpResponse(form_html + content)
+	
+	with open(settings.BASE_DIR + "/wpsblog/templates/news.html","r") as template:
+		content = template.read()
+		content += "".join([
+				"<h2>{title}</h2><img src={image_url}><p>{content}</p>".format(
+					title=news.get("title"),
+					image_url=news.get('image'),
+					content=news.get('content'),
+					)
+				for news in news_list
+				])
+	return HttpResponse(content)
