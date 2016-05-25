927b63d4299fcef9f0f16a5d488faf1a3687b3e4
SeunghwanJoo <tmdghks584@gmail.com>
2016-05-25 14:28:28 +0900

    add news controller

--- wpsblog/wpsblog/urls.py
+++ wpsblog/wpsblog/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from wpsblog.views import home, room
+from wpsblog.views import home, room, news
 
 
 urlpatterns = [
@@ -23,4 +23,5 @@ urlpatterns = [
 
     url(r'^$', home),
     url(r'^rooms/(?P<room_id>\d+)/$',room),
+    url(r'^news/', news),
 ]
--- wpsblog/wpsblog/views.py
+++ wpsblog/wpsblog/views.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from django.http.response import HttpResponse
 
@@ -7,11 +8,41 @@ def home(request):
     return HttpResponse("hello world")
 
 def room(request, room_id):
-    # 방 번호 ( room_id) 직방의 데이터를 그대로 보여주는 뷰(컨트롤러)
-    url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
-    response = requests.get(url)
-    return HttpResponse(
-        response.content,
-        content_type="application/json",
-    )
+	# 방 번호 ( room_id) 직방의 데이터를 그대로 보여주는 뷰(컨트롤러)
+	url = "https://api.zigbang.com/v1/items?detail=true&item_ids=" + room_id
+	response = requests.get(url)
+	return HttpResponse(
+		response.content,
+		content_type="application/json",
+	)
 
+def news(request):
+
+	search = request.GET.get('search')
+	response = requests.get("https://watcha.net/home/news.json?page=1&per=50")
+	news_dict = json.loads(response.text)
+	news_list = news_dict.get("news")
+
+	form_html = """
+		<form method="GET" action="/news/">
+			<input type="text" name="search">
+			<input type="submit" value="검색">
+		</form>
+	"""
+
+	if search:
+		news_list = list(filter(
+				lambda news: search in news.get('title'),
+				news_list,
+		))
+	content = "<h1>News</h1>" +\
+			   "<p>This is news page.</p>" +\
+			   "".join([
+					   "<h2>{title}</h2><img src={image_url}><p>{content}</p>".format(
+						   title=news.get("title"),
+						   image_url=news.get('image'),
+						   content=news.get('content'),
+						   )
+					   for news in news_list
+					   ])
+	return HttpResponse(form_html + content)
